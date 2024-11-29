085e4f2b0421cac5b889cc8e374003d9f1705c86
binh234 <binhnd234@gmail.com>
2024-11-29 04:50:57 -0500

    Update script

--- auto_nessus.py
+++ auto_nessus.py
@@ -4,15 +4,16 @@ import os
 import time
 
 import pandas as pd
-from tenable.io import TenableIO
+from tenable.nessus import Nessus
 from tqdm import tqdm
 from dotenv import load_dotenv
+import uuid
 
 load_dotenv()
 access_key = os.getenv("ACCESS_KEY")
-secret_key = os.getenv("SERET_KEY")
+secret_key = os.getenv("SECRET_KEY")
 # Initialize Tenable.io client
-tio = TenableIO(access_key, secret_key)
+nessus = Nessus(access_key=access_key, secret_key=secret_key, url="https://127.0.0.1:8834")
 
 logging.basicConfig(
     level=logging.INFO,
@@ -41,10 +42,10 @@ def main(args):
     for (username, password, method), group in grouped:
         ips = group["ip"].tolist()
         ip_list = ",".join(ips)
-        scan_name = group.iloc[0]["name"] + ip_list
+        scan_name = group.iloc[0]["name"] + "-" + ip_list
         logging.info(f"Creating scan for IPs: {ip_list}")
 
-        scan_id = create_and_launch_scan(scan_name, ips, username, password, method, folder_id)
+        scan_id = create_and_launch_scan(scan_name, ip_list, username, password, method, folder_id)
         if scan_id:
             scan_ids.append(scan_id)
 
@@ -55,36 +56,30 @@ def main(args):
 
 
 def create_folder(folder_name):
-    folders = tio.folders.list()
+    folders = nessus.folders.list()
     for folder in folders:
         if folder["name"] == folder_name:
             logging.info(f"Found existing folder {folder_name}")
             return folder["id"]
 
     logging.info(f"Creating new folder {folder_name}")
-    new_folder = tio.folders.create(folder_name)
-    return new_folder["id"]
+    new_folder_id = nessus.folders.create(folder_name)
+    return new_folder_id
 
 
-def create_and_launch_scan(scan_name, ips, username, password, method, folder_id):
+def create_and_launch_scan(scan_name, ip_list, username, password, method, folder_id):
     # Prepare the credentials based on the authentication method
     if method.lower() == "ssh":
         credentials = {
+            "add": {
             "Host": {
-                "SSH": [{"username": username, "password": password, "auth_method": "password"}]
-            }
+                "SSH": [{"auth_method":"password","username":username,"password":password,"elevate_privileges_with":"Nothing","custom_password_prompt":"","target_priority_list":""}]
+            }}
         }
     elif method.lower() == "http":
         credentials = {
-            "Plaintext Authentication": {
-                "HTTP": [
-                    {
-                        "username": username,
-                        "password": password,
-                        "auth_method": "Automatic authentication",
-                    }
-                ]
-            }
+            "add": {
+            "Plaintext Authentication":{"HTTP":[{"auth_method":"Automatic authentication","username":username,"password":password}]}}
         }
     else:
         logging.error(
@@ -93,29 +88,32 @@ def create_and_launch_scan(scan_name, ips, username, password, method, folder_id
         return None
     
     # Create and launch scan
-    scan = tio.scans.create(
-        scan_name,
-        targets=ips,
-        folder_id=folder_id,
+    scan_uuid = uuid.uuid4()
+    scan = nessus.scans.create(
+        uuid="731a8e52-3ea6-a291-ec0a-d2ff0619c19d7bd788d6be818b65",
+        settings={
+            "name": scan_name,
+            "folder_id": folder_id,
+            "text_targets": ip_list,
+        },
         credentials=credentials,
     )
-    tio.scans.launch(scan["id"])
-    return scan["id"]
-
+    scan_id = scan["scan"]["id"]
+    nessus.scans.launch(scan_id)
+    return scan_id
 
 def download_report(scan_id, scan_name):
 
     while True:
-        status = tio.scans.status(scan_id)
-        if status == "completed":
+        details = nessus.scans.details(scan_id)
+        if details["status"] == "completed":
             break
         time.sleep(10)
 
     # Download the report
     report_path = f"./{scan_name}_report.html"
-    report_file = open(report_path, "wb")
-    tio.scans.export(scan_id, format="html", fobj=report_file)
-    report_file.close()
+    with open(report_path, "wb") as f:
+        nessus.scans.export_scan(scan_id, format="html", fobj=f)
     logging.info(f"Report downloaded: {report_path}")
 
 
@@ -127,9 +125,9 @@ if __name__ == "__main__":
         "file", type=str, help="Path to the Excel or CSV file containing credentials."
     )
     parser.add_argument(
-        "--name", "-n", type=str, help="Name of the Nessus folder to use or create."
+        "--name", "-n", type=str, required=True, help="Name of the Nessus folder to use or create."
     )
-    parser.add_argument("--delimiter", "-d", type=str, help="CSV delimiter")
+    parser.add_argument("--delimiter", "-d", type=str, default=",", help="CSV delimiter")
     parser.add_argument(
         "--output", "-o", type=str, default=None, help="Output folder to save reports"
     )
