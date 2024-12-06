8cbcf053fef983d49a398e534302970ff4a809a4
binh234 <binhnd234@gmail.com>
2024-12-05 23:07:30 -0500

    Add unidecode lib to sanitize scan name, remove group IP by cred, support max scans run concurrently

--- auto_nessus.py
+++ auto_nessus.py
@@ -7,6 +7,7 @@ import coloredlogs
 import pandas as pd
 from dotenv import load_dotenv
 from tenable.nessus import Nessus
+from unidecode import unidecode
 
 load_dotenv()
 
@@ -35,29 +36,37 @@ def main(args):
         print("Unsupported file format. Please provide a CSV or Excel file.")
         return
 
+    if args.save:
+        logger.info(f"Scan reports will be downloaed to directory {args.name}")
+        os.makedirs(args.name, exist_ok=True)
+
     # Lower all column names
     data.columns = data.columns.str.lower()
     if "username/password" in data.columns:
         data[["username", "password"]] = data["username/password"].str.split(args.upd, expand=True)
 
     data["method"] = data["method"].fillna("ssh")  # Default authentication method to ssh
-    grouped = data.groupby(["username", "password", "method"])
+    data["name"] = data["name"].map(unidecode)
 
     folder_id = create_folder(args.name)
     scan_recs = []
-    for (username, password, method), group in grouped:
-        ips = group["ip"].tolist()
-        ip_list = ",".join(ips)
-        scan_name = group.iloc[0]["name"] + "-" + ip_list
+    for _, row in data.iterrows():
+        username = row["username"]
+        password = row["password"]
+        method = row["method"]
+        ip_list = row["ip"]
+        scan_name = row["name"] + "-" + ip_list
         logger.info(f"Creating scan for IPs: {ip_list}")
 
         scan_id = create_and_launch_scan(scan_name, ip_list, username, password, method, folder_id)
         if scan_id:
             scan_recs.append((scan_id, scan_name))
 
-    if args.output is not None:
-        os.makedirs(args.output, exist_ok=True)
-        download_report(args.output, scan_recs)
+        if len(scan_recs) >= args.max:
+            complete_idx = check_completed_scan_idx(scan_recs)
+            complete_id, complete_name = scan_recs.pop(complete_idx)
+            if args.save:
+                download_report(args.name, complete_id, complete_name)
 
 
 def create_folder(folder_name):
@@ -105,6 +114,21 @@ def create_and_launch_scan(scan_name, ip_list, username, password, method, folde
                 }
             }
         }
+    elif method.lower() == "windows":
+        credentials = {
+            "add": {
+                "Host": {
+                    "Windows": [
+                        {
+                            "auth_method": "Password",
+                            "username": username,
+                            "password": password,
+                            "domain": "",
+                        }
+                    ]
+                }
+            }
+        }
     else:
         logger.error(
             f"Unsupported authentication method: {method}. Skipping scan creation for {scan_name}."
@@ -126,15 +150,27 @@ def create_and_launch_scan(scan_name, ip_list, username, password, method, folde
     return scan_id
 
 
-def download_report(output, scans):
-    """Download Nessus scan report
+def check_completed_scan_idx(scans):
+    complete_idx = None
+    while complete_idx is None:
+        for idx, (scan_id, scan_name) in enumerate(scans):
+            details = nessus.scans.details(scan_id)
+            if details["info"]["status"] == "completed":
+                logger.info(f"Scan {scan_name} finished")
+                complete_idx = idx
+                return complete_idx
+            else:
+                time.sleep(15)
+
+
+def download_report(output, scan_id, scan_name):
+    """Download Nessus scan report for scan ID
 
     Args:
         output (str): Output directory
-        scans (List[Tuple]): List of (scan_id, scan_name) pairs
+        scan_id (int): Scan ID
+        scan_name (str): Scan name
     """
-
-    logger.info("Downloading scan reports..")
 
     # Retrieve template id
     templates = nessus.get("reports/custom/templates", box=True)
@@ -149,23 +185,15 @@ def download_report(output, scans):
         logger.error(f"Can not find template id for {template_name}")
         return
 
-    while len(scans) > 0:
-        next_scans = []
-        for scan_id, scan_name in scans:
-            details = nessus.scans.details(scan_id)
-            if details["info"]["status"] == "completed":
-                # Download the report
-                report_path = os.path.join(output, f"{scan_name}_report.html")
-                with open(report_path, "wb") as f:
-                    nessus.scans.export_scan(
-                        scan_id, format="html", template_id=template_id, fobj=f
-                    )
-                logger.info(f"Report downloaded: {report_path}")
-            else:
-                next_scans.append((scan_id, scan_name))
-                time.sleep(15)
-
-        scans = next_scans
+    details = nessus.scans.details(scan_id)
+    if details["info"]["status"] == "completed":
+        # Download the report
+        report_path = os.path.join(output, f"{scan_name}_report.html")
+        with open(report_path, "wb") as f:
+            nessus.scans.export_scan(scan_id, format="html", template_id=template_id, fobj=f)
+        logger.info(f"Report downloaded: {report_path}")
+    else:
+        logger.error(f"Scan {scan_name} is not finished yet!")
 
 
 if __name__ == "__main__":
@@ -182,17 +210,24 @@ if __name__ == "__main__":
         "--max",
         "-m",
         type=int,
-        default=10,
+        default=8,
         help="Maximum scans to run cocurrently",
     )
     parser.add_argument(
         "--delimiter", "-d", type=str, default=",", help="CSV delimiter, defaults to ','"
     )
     parser.add_argument(
-        "--upd", type=str, default="/", help="Username/password delimiter, defaults to '/'"
+        "--upd",
+        type=str,
+        default="/",
+        help="Username/password delimiter, defaults to '/'. Only affect when 'Username/password' column exists",
     )
     parser.add_argument(
-        "--output", "-o", type=str, default=None, help="Output folder to save reports"
+        "--save",
+        "-s",
+        default=False,
+        action="store_true",
+        help="Whether to save reports in local directory",
     )
 
     args = parser.parse_args()
